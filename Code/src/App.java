396b192d8208a857ed47162f8255d0ef48642424
AsmaB20 <162128276+AsmaB20@users.noreply.github.com>
2025-11-18 00:37:34 +0300

    Fixed launching error
    Fixed launching error by adding necessary files and libraries, and importing Scene in App.java

--- Code/src/App.java
+++ Code/src/App.java
@@ -1,5 +1,8 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+
 public class App extends Application {
 
     @Override
@@ -10,7 +13,7 @@ public class App extends Application {
     }
 
     public static void main(String[] args) throws Exception{
-        launch(args);
+        launch();
     }
 
     public static void launch(String[] args) {
